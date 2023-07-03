b706472b0ba1112edb044366d8205bed3692e342
Choi YoungLyn <joeuni0129@gmail.com>
2023-07-04 02:20:45 +0900

    [Setting] #3 - 컬러 추가

--- Umbba-iOS/Umbba-iOS/Global/Literals/ColorLiterals.swift
+++ Umbba-iOS/Umbba-iOS/Global/Literals/ColorLiterals.swift
@@ -5,4 +5,46 @@
 //  Created by 최영린 on 2023/07/03.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static var Primary400: UIColor {
+        return UIColor(hex: "#F1BD9B")
+    }
+    
+    static var Primary500: UIColor {
+        return UIColor(hex: "#FE99F7A")
+    }
+    
+    static var Primary600: UIColor {
+        return UIColor(hex: "#C87759")
+    }
+    
+    static var Gray300: UIColor {
+        return UIColor(hex: "#F7F2F0")
+    }
+    
+    static var Gray400: UIColor {
+        return UIColor(hex: "#F0EBE8")
+    }
+    
+    static var Gray500: UIColor {
+        return UIColor(hex: "#E7E0DC")
+    }
+
+    static var Error: UIColor {
+        return UIColor(hex: "#FF3A1F")
+    }
+    
+    static var Success: UIColor {
+        return UIColor(hex: "#419277")
+    }
+    
+    static var UmbbaBlack: UIColor {
+        return UIColor(hex: "#32211B")
+    }
+    
+    static var UmbbaWhite: UIColor {
+        return UIColor(hex: "#FCFAF8")
+    }
+}
