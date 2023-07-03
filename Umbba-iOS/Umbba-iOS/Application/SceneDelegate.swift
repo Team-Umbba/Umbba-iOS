dd7233ed80c41da3c622f0b17c12cb664031d714
Choi YoungLyn <joeuni0129@gmail.com>
2023-07-03 23:44:47 +0900

    [Setting] #1 - swiftLint 설정

--- Umbba-iOS/Umbba-iOS/Application/SceneDelegate.swift
+++ Umbba-iOS/Umbba-iOS/Application/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
-    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
@@ -46,7 +45,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-    
-    
-}
 
+}
