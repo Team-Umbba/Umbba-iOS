9fd98701f35b4ba4669dbc6a7a89247968992f5a
Choi YoungLyn <joeuni0129@gmail.com>
2023-07-04 21:50:30 +0900

    [Add] #8 - 정규식 모음 추가

---
+++ Umbba-iOS/Umbba-iOS/Global/Extension/String+.swift
@@ -0,0 +1,40 @@
+//
+//  String+.swift
+//  Umbba-iOS
+//
+//  Created by 최영린 on 2023/07/04.
+//
+
+import UIKit
+
+extension String {
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+    
+    func isContainEnglish() -> Bool {
+        let pattern = "[A-Za-z]+"
+        guard self.range(of: pattern, options: .regularExpression) != nil else { return false}
+        return true
+    }
+    
+    func isContainNumber() -> Bool {
+        let pattern = ".*[0-9]+.*"
+        guard self.range(of: pattern, options: .regularExpression) != nil else { return false}
+        return true
+    }
+    
+    func isContainNumberAndAlphabet() -> Bool {
+        let pattern = "^[0-9a-zA-Z]*$"
+        guard self.range(of: pattern, options: .regularExpression) != nil else { return false}
+        return true
+    }
+    
+    func isOnlyKorean() -> Bool {
+        let pattern = "^[가-힣ㄱ-ㅎㅏ-ㅣ]+$"
+        guard self.range(of: pattern, options: .regularExpression) != nil else { return false }
+        return true
+    }
+}
