ed7056a1d13d9f1ba03cfa330c0a326eb0d991fc
Choi YoungLyn <joeuni0129@gmail.com>
2023-07-04 21:52:59 +0900

    [Feat] #8 - 초대코드입력 UI 구현

--- Umbba-iOS/Umbba-iOS/Global/Extension/UILabel+.swift
+++ Umbba-iOS/Umbba-iOS/Global/Extension/UILabel+.swift
@@ -129,4 +129,19 @@ extension UILabel {
         self.attributedText = attributedString
     }
     
+    /// 라벨 일부 font 변경하면서 행간도 조정하는 함수
+    /// - lineSpacing에는 적용하고자 하는 값을 넣어주세요
+    /// - targerString에는 바꾸고자 하는 특정 문자열을 넣어주세요
+    /// - font에는 targetString에 적용하고자 하는 UIFont를 넣어주세요
+    func setLineSpacingPartFontChange(lineSpacing: CGFloat, targetString: String, font: UIFont) {
+        let fullText = self.text ?? ""
+        let range = (fullText as NSString).range(of: targetString)
+        let attributedString = NSMutableAttributedString(string: fullText)
+        attributedString.addAttribute(.font, value: font, range: range)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+        attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+        self.attributedText = attributedString
+    }
+        
 }
