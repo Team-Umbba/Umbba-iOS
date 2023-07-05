b60c64d0cf30c01416ea3de464bf1b413958a140
ahra1221 <lizze011221@gmail.com>
2023-07-05 14:48:08 +0900

    [Feat] #10 - EntryViewController 구현

---
+++ Umbba-iOS/Umbba-iOS/Presentation/Auth/EntryScene/ViewController/EntryViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EntryViewController.swift
+//  Umbba-iOS
+//
+//  Created by 고아라 on 2023/07/05.
+//
+
+import UIKit
+
+final class EntryViewController: UIViewController {
+
+    private let entryView = EntryView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func loadView() {
+        super.loadView()
+        self.view = entryView
+    }
+
+}
