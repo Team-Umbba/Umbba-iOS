05fb210832f5981c9de4eaec255a3ef29cbd0f60
ahra1221 <lizze011221@gmail.com>
2023-07-05 14:47:21 +0900

    [Add] #10 - StringLiteral 추가

--- Umbba-iOS/Umbba-iOS/Global/Literals/StringLiterals.swift
+++ Umbba-iOS/Umbba-iOS/Global/Literals/StringLiterals.swift
@@ -17,6 +17,10 @@ struct I18N {
         static let loginTitle = "부모와 떠나는 시간 여행"
         static let appleButtonTitle = "애플로 로그인"
         static let kakaoButtonTitle = "카카오로 로그인"
+        static let entryButtonTitle = "시작하기"
+        static let dividingText = "--------------------- or ---------------------"
+        static let inviteText = "초대 코드를 받으셨나요?"
+        static let inviteButtonTitle = "초대코드 입력하기"
     }
     
 }
