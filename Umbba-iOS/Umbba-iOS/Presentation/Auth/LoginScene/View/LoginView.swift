ebd97e0a40355e5003798c18468901c6ca1587e0
ahra1221 <lizze011221@gmail.com>
2023-07-07 01:40:32 +0900

    [Fix] #10 - 버튼 기기대응 수정

--- Umbba-iOS/Umbba-iOS/Presentation/Auth/LoginScene/View/LoginView.swift
+++ Umbba-iOS/Umbba-iOS/Presentation/Auth/LoginScene/View/LoginView.swift
@@ -11,6 +11,8 @@ import SnapKit
 
 final class LoginView: UIView {
     
+    let screenSize = UIScreen.main.bounds.width
+    
     private lazy var loginViewImage: UIImageView = {
         let image = UIImageView()
         return image
@@ -25,19 +27,50 @@ final class LoginView: UIView {
     }()
     
     lazy var loginAppleButton: UIButton = {
-        let button = CustomButton(status: false, title: I18N.Auth.appleButtonTitle)
+        var config = UIButton.Configuration.plain()
+        var title = AttributedString.init(I18N.Auth.appleButtonTitle)
+        title.font = .PretendardBold(size: 16)
+        title.foregroundColor = .UmbbaWhite
+        config.attributedTitle = title
+        
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 24)
+        let image = UIImage(systemName: "apple.logo", withConfiguration: imageConfig)
+        config.image = image
+        config.imagePadding = 72 / 375 * screenSize
+        config.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0)
+        
+        let button = UIButton(configuration: config)
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 30
+        button.contentHorizontalAlignment = .left
+        
         return button
     }()
     
     lazy var loginKakaoButton: UIButton = {
-        let button = CustomButton(status: false, title: I18N.Auth.kakaoButtonTitle)
+        var config = UIButton.Configuration.plain()
+        var title = AttributedString.init(I18N.Auth.kakaoButtonTitle)
+        title.font = .PretendardBold(size: 16)
+        title.foregroundColor = .UmbbaWhite
+        config.attributedTitle = title
+        
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 24)
+        let image = UIImage(systemName: "message.fill", withConfiguration: imageConfig)
+        config.image = image
+        config.imagePadding = 72 / 375 * screenSize
+        config.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0)
+        
+        let button = UIButton(configuration: config)
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 30
+        button.contentHorizontalAlignment = .left
         return button
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        // MARK: - 컴포넌트 설정
+        // MARK: - UI Components
         setUI()
         
         // MARK: - addsubView
@@ -90,5 +123,4 @@ extension LoginView {
             $0.height.equalTo(60)
         }
     }
-
 }
