b94ca2605cda30932056a90a30a1a078bfdb6d66
ahra1221 <lizze011221@gmail.com>
2023-07-09 15:45:26 +0900

    [Feat] #16 - ArchivingSectionCollectionViewCell 구현

---
+++ Umbba-iOS/Umbba-iOS/Presentation/Archiving/ArchivingScene/Cell/ArchivingSectionCollectionViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  ArchivingSectionCollectionViewCell.swift
+//  Umbba-iOS
+//
+//  Created by 고아라 on 2023/07/09.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ArchivingSectionCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ArchivingSectionCollectionViewCell"
+    
+    private let archivingSectionLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .PretendardRegular(size: 12)
+        label.textColor = .UmbbaWhite
+        label.layer.borderWidth = 1
+        label.layer.borderColor = UIColor.gray.cgColor
+        label.backgroundColor = .gray
+        label.clipsToBounds = true
+        label.layer.cornerRadius = 12
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        
+        // MARK: - addsubView
+        setHierarchy()
+        
+        setLayout()
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ArchivingSectionCollectionViewCell {
+    
+    func setUI() {
+        backgroundColor = .UmbbaWhite
+    }
+    
+    func setHierarchy() {
+        addSubview(archivingSectionLabel)
+    }
+    
+    func setLayout() {
+        archivingSectionLabel.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(26)
+        }
+    }
+    
+    func setDataBind(model: ArchivingSectionItem) {
+        archivingSectionLabel.text = model.keywordTitle
+    }
+}
