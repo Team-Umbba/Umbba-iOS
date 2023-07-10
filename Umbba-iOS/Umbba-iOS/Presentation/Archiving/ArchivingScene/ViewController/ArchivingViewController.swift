159b053848790cf4c24d9bb6e2a868f0130dc0eb
ahra1221 <lizze011221@gmail.com>
2023-07-11 03:15:16 +0900

    [Feat] #16 - ArchivingViewController 구현

--- Umbba-iOS/Umbba-iOS/Presentation/Archiving/ArchivingScene/ViewController/ArchivingViewController.swift
+++ Umbba-iOS/Umbba-iOS/Presentation/Archiving/ArchivingScene/ViewController/ArchivingViewController.swift
@@ -18,11 +18,18 @@ final class ArchivingViewController: UIViewController {
         case section, question
     }
     
+    // MARK: - UI Components
+    
     private let archivingImageView = ArchivingImageView()
-    private lazy var archivingCollectionView = UICollectionView(frame: .zero, collectionViewLayout: self.setSectionLayout())
+    private let archivingCollectionView = ArchivingCollectionView()
+    private lazy var collectionView = archivingCollectionView.ArchivingCollectionView
     
     private let archivingSectionModel: [ArchivingSectionItem] = ArchivingSectionItem.archivingKeywordData()
     private let archivingQuestionModel: [ArchivingQuestionItem] = ArchivingQuestionItem.archivingQuestionDummy()
+    
+    private var selectedSectionIndexPath: Int?
+    
+    // MARK: - Life Cycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,26 +38,20 @@ final class ArchivingViewController: UIViewController {
         setDelegate()
         setHierarchy()
         setLayout()
-        registerCell()
         setNavigationUI()
     }
 }
+
+// MARK: - Extensions
 
 extension ArchivingViewController {
     
     private func setUI() {
         view.backgroundColor = .UmbbaWhite
-        archivingCollectionView.showsVerticalScrollIndicator = false
-        archivingCollectionView.isScrollEnabled = true
-        archivingCollectionView.clipsToBounds = true
-        archivingCollectionView.contentInsetAdjustmentBehavior = .never
-        archivingCollectionView.collectionViewLayout = self.setSectionLayout()
-        archivingCollectionView.isUserInteractionEnabled = true
-        archivingCollectionView.allowsSelection = true
     }
     
     private func setHierarchy() {
-        view.addSubviews(archivingImageView, archivingCollectionView)
+        view.addSubviews(archivingImageView, collectionView)
     }
     
     private func setLayout() {
@@ -58,98 +59,21 @@ extension ArchivingViewController {
             $0.top.leading.trailing.equalToSuperview()
         }
         
-        archivingCollectionView.snp.makeConstraints {
+        collectionView.snp.makeConstraints {
             $0.top.equalTo(archivingImageView.snp.bottom).offset(12)
             $0.leading.trailing.bottom.equalToSuperview()
         }
     }
     
     private func setDelegate() {
-        archivingCollectionView.delegate = self
-        archivingCollectionView.dataSource = self
-    }
-    
-    private func registerCell() {
-        archivingCollectionView.register(ArchivingSectionCollectionViewCell.self, forCellWithReuseIdentifier: ArchivingSectionCollectionViewCell.identifier)
-        archivingCollectionView.register(ArchivingQuestionCollectionViewCell.self, forCellWithReuseIdentifier: ArchivingQuestionCollectionViewCell.identifier)
-        
-        archivingCollectionView.register(ArchivingQuestionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ArchivingQuestionHeaderView")
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     func setNavigationUI() {
         navigationItem.title = I18N.Archiving.navigationTitle
-        navigationItem.leftBarButtonItem?.tintColor = .UmbbaBlack
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.PretendardRegular(size: 16), .foregroundColor: UIColor.UmbbaBlack]
     }
-    
-    func setSectionLayout() -> UICollectionViewLayout {
-        return UICollectionViewCompositionalLayout { sectionIndex, env -> NSCollectionLayoutSection? in
-            let sectionType = Section.allCases[sectionIndex]
-            switch sectionType {
-            case .section:
-                return self.getLayoutSection()
-            case .question:
-                return self.getLayoutQuestion()
-            }
-        }
-    }
-    
-    func getLayoutSection() -> NSCollectionLayoutSection {
-        let itemSize = NSCollectionLayoutSize(
-            widthDimension: .estimated(100),
-            heightDimension: .absolute(26)
-        )
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.5),
-            heightDimension: .absolute(26)
-        )
-        let group = NSCollectionLayoutGroup.horizontal(
-            layoutSize: groupSize,
-            subitems: [item]
-        )
-        group.interItemSpacing = .fixed(12)
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 24, bottom: 8, trailing: 0)
-        section.orthogonalScrollingBehavior = .continuous
-        
-        return section
-    }
-    
-    func getLayoutQuestion() -> NSCollectionLayoutSection {
-        let itemSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.0),
-            heightDimension: .absolute(60)
-        )
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.0),
-            heightDimension: .absolute(60)
-        )
-        let group = NSCollectionLayoutGroup.horizontal(
-            layoutSize: groupSize,
-            subitems: [item]
-        )
-        
-        let headerSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.0),
-            heightDimension: .absolute(48)
-        )
-        let header = NSCollectionLayoutBoundarySupplementaryItem(
-            layoutSize: headerSize,
-            elementKind: UICollectionView.elementKindSectionHeader,
-            alignment: .top
-        )
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .none
-        section.boundarySupplementaryItems = [header]
-        
-        return section
-    }
-    
 }
 
 extension ArchivingViewController: UICollectionViewDelegate {
@@ -157,28 +81,46 @@ extension ArchivingViewController: UICollectionViewDelegate {
         let sectionType = Section.allCases[indexPath.section]
         switch sectionType {
         case .section:
-            print("asdf")
+            selectedSectionIndexPath = indexPath.row
+            if let cell = collectionView.cellForItem(at: indexPath) as? ArchivingSectionCollectionViewCell {
+                cell.backgroundColor = .Primary600
+                cell.archivingSectionLabel.textColor = .UmbbaWhite
+            }
         case .question:
-            print("iroi")
+            break
         }
     }
     
-    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        print("shouldSelectItemAt")
-        return true
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let sectionType = Section.allCases[indexPath.section]
+        switch sectionType {
+        case .section:
+            if let cell = collectionView.cellForItem(at: indexPath) as? ArchivingSectionCollectionViewCell {
+                cell.backgroundColor = .UmbbaWhite
+                cell.archivingSectionLabel.textColor = .Primary600
+            }
+        case .question:
+            break
+        }
     }
 }
 
 extension ArchivingViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let sectionType = Section.allCases[indexPath.section]
+        let sectionType = SectionType.allCases[indexPath.section]
         switch sectionType {
         case .section:
-            guard let cell = archivingCollectionView.dequeueReusableCell(withReuseIdentifier: ArchivingSectionCollectionViewCell.identifier, for: indexPath) as? ArchivingSectionCollectionViewCell else { return UICollectionViewCell() }
+            let cell =
+                    ArchivingSectionCollectionViewCell.dequeueReusableCell(collectionView: collectionView, indexPath: indexPath)
             cell.setDataBind(model: archivingSectionModel[indexPath.row])
+            if indexPath.item == 1 {
+                cell.backgroundColor = .Primary600
+                cell.archivingSectionLabel.textColor = .UmbbaWhite
+                collectionView.selectItem(at: indexPath, animated: false, scrollPosition: .init())
+            }
             return cell
         case .question:
-            guard let cell = archivingCollectionView.dequeueReusableCell(withReuseIdentifier: ArchivingQuestionCollectionViewCell.identifier, for: indexPath) as? ArchivingQuestionCollectionViewCell else { return UICollectionViewCell() }
+            let cell = ArchivingQuestionCollectionViewCell.dequeueReusableCell(collectionView: collectionView, indexPath: indexPath)
             cell.setDataBind(model: archivingQuestionModel[indexPath.row])
             return cell
         }
@@ -201,7 +143,8 @@ extension ArchivingViewController: UICollectionViewDataSource {
             let view = UICollectionReusableView()
             return view
         case .question:
-            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ArchivingQuestionHeaderView", for: indexPath) as! ArchivingQuestionHeaderView
+            let headerView = ArchivingQuestionHeaderView.dequeueReusableHeaderView(collectionView: collectionView, indexPath: indexPath)
+            headerView.setDataBind(model: archivingQuestionModel[indexPath.row])
             return headerView
         }
     }
