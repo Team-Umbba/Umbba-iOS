7f4b8465de113c1b74cdfd67a4bb4666cd285dbc
ahra1221 <lizze011221@gmail.com>
2023-07-11 03:14:54 +0900

    [Feat] #16 - ArchivingCollectionView 구현

---
+++ Umbba-iOS/Umbba-iOS/Presentation/Archiving/ArchivingScene/View/ArchivingCollectionView.swift
@@ -0,0 +1,142 @@
+//
+//  ArchivingCollectionView.swift
+//  Umbba-iOS
+//
+//  Created by 고아라 on 2023/07/10.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ArchivingCollectionView: UIView {
+    
+    private typealias SectionType = Section
+    
+    @frozen
+    private enum Section: CaseIterable {
+        case section, question
+    }
+    
+    // MARK: - UI Components
+    
+    lazy var ArchivingCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.setSectionLayout())
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.isScrollEnabled = false
+        collectionView.clipsToBounds = true
+        collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.collectionViewLayout = self.setSectionLayout()
+        collectionView.isUserInteractionEnabled = true
+        collectionView.allowsSelection = true
+        collectionView.backgroundColor = .UmbbaWhite
+        return collectionView
+    }()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setHierarchy()
+        setLayout()
+        registerCell()
+
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+// MARK: - Extensions
+
+private extension ArchivingCollectionView {
+        
+    func setHierarchy() {
+        self.addSubview(ArchivingCollectionView)
+    }
+    
+    func setLayout() {
+        ArchivingCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func registerCell() {
+        ArchivingSectionCollectionViewCell.register(target: ArchivingCollectionView)
+        ArchivingQuestionCollectionViewCell.register(target: ArchivingCollectionView)
+        ArchivingQuestionHeaderView.register(target: ArchivingCollectionView)
+    }
+    
+    func setSectionLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { sectionIndex, env -> NSCollectionLayoutSection? in
+            let sectionType = Section.allCases[sectionIndex]
+            switch sectionType {
+            case .section:
+                return self.getLayoutSection()
+            case .question:
+                return self.getLayoutQuestion()
+            }
+        }
+    }
+    
+    func getLayoutSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .estimated(100),
+            heightDimension: .absolute(26)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.5),
+            heightDimension: .absolute(26)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        group.interItemSpacing = .fixed(12)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 24, bottom: 8, trailing: 0)
+        section.orthogonalScrollingBehavior = .continuous
+        
+        return section
+    }
+    
+    func getLayoutQuestion() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(60)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(60)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(48)
+        )
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .none
+        section.boundarySupplementaryItems = [header]
+        
+        return section
+    }
+}
