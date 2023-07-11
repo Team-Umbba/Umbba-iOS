ad17d5393fc63098dcf68da8648375a1ba6767d8
yujin-00 <55316673+yujin-00@users.noreply.github.com>
2023-07-11 15:38:02 +0900

    [Del] #29 - 테이블뷰 title String 파일 내용 삭제

--- Umbba-iOS/Umbba-iOS/Presentation/Setting/SettingScene/Setting.swift
+++ Umbba-iOS/Umbba-iOS/Presentation/Setting/SettingScene/Setting.swift
@@ -6,22 +6,3 @@
 //
 
 import UIKit
-
-struct Setting {
-    let title: String
-}
-
-extension Setting {
-    
-    static func section0() -> [Setting] {
-        return [Setting(title: "계정 관리")
-        ]
-    }
-    
-    static func section1() -> [Setting] {
-        return [Setting(title: "About 엄빠도 어렸다"),
-                Setting(title: "이용약관"),
-                Setting(title: "공지사항")
-        ]
-    }
-}
