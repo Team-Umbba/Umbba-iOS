93e418cac99fc75521d41f96c862012e34245b00
Choi YoungLyn <joeuni0129@gmail.com>
2023-07-11 22:02:51 +0900

    [Feat] #41 - NavigationBar BackButton 이벤트 구현

--- Umbba-iOS/Umbba-iOS/Presentation/Onboarding/NoticeAlarmScene/ViewController/NoticeAlarmViewController.swift
+++ Umbba-iOS/Umbba-iOS/Presentation/Onboarding/NoticeAlarmScene/ViewController/NoticeAlarmViewController.swift
@@ -23,8 +23,8 @@ final class NoticeAlarmViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setNavigationUI()
+      
+        setDelegate()
         setAddTarget()
     }
 }
@@ -32,23 +32,29 @@ final class NoticeAlarmViewController: UIViewController {
 // MARK: - Extensions
 
 private extension NoticeAlarmViewController {
-    func setNavigationUI() {
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.backward"), style: .plain, target: self, action: #selector(backButtonTapped))
-        navigationItem.leftBarButtonItem?.tintColor = .UmbbaBlack
+
+    func setDelegate() {
+        noticeAlarmView.delegate = self
     }
     
     func setAddTarget() {
         nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
-    }
-
-    @objc
-    func backButtonTapped() {
-        print("이전 화면으로 전환")
     }
     
     @objc
     func nextButtonTapped() {
 //        self.navigationController?.pushViewController(InviteViewController(), animated: true)
         print("다음 화면으로 전환")
+    }
+}
+
+extension NoticeAlarmViewController: NavigationBarDelegate {
+    func backButtonTapped() {
+        print("이전화면으로 이동")
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func completeButtonTapped() {
+
     }
 }
