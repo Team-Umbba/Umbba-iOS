50766924994d0c415343e85583920eefac34cf15
yujin-00 <55316673+yujin-00@users.noreply.github.com>
2023-07-11 19:31:43 +0900

    [Feat] #39 - AccountTableView 구현

--- Umbba-iOS/Umbba-iOS/Presentation/Setting/SettingScene/View/AccountTableView.swift
+++ Umbba-iOS/Umbba-iOS/Presentation/Setting/SettingScene/View/AccountTableView.swift
@@ -5,4 +5,54 @@
 //  Created by 남유진 on 2023/07/11.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class AccountTableView: UIView {
+    
+    // MARK: - UI Components
+    
+    lazy var tableView = UITableView(frame: .zero, style: .plain)
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+        setTableView()
+        registerCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+private extension AccountTableView {
+    
+    func setUI() {
+        backgroundColor = .Gray400
+    }
+    
+    func setLayout() {
+        addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func setTableView() {
+        tableView.backgroundColor = .Gray400
+    }
+    
+    func registerCell() {
+        SettingTableViewCell.register(target: tableView)
+    }
+}
+
