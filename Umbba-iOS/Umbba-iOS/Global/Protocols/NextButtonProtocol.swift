892059160342d145e92c2dbb41f0e7b99163d1eb
Choi YoungLyn <joeuni0129@gmail.com>
2023-07-11 22:09:21 +0900

    [Add] #41 - NextButtonDelegate Protocol 추가

---
+++ Umbba-iOS/Umbba-iOS/Global/Protocols/NextButtonProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  NextButtonProtocol.swift
+//  Umbba-iOS
+//
+//  Created by 최영린 on 2023/07/11.
+//
+
+import Foundation
+
+protocol NextButtonDelegate: AnyObject {
+    func nextButtonTapped()
+}
