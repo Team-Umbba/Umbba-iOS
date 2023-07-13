b01e3141d98fe2d41a8c6c0bb12b7b74cf23b7fd
ahra1221 <lizze011221@gmail.com>
2023-07-13 16:55:41 +0900

    [Feat] #53 - LottieViewController 구현

---
+++ Umbba-iOS/Umbba-iOS/Global/Resources/Lottie/LottieViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LottieViewController.swift
+//  Umbba-iOS
+//
+//  Created by 고아라 on 2023/07/13.
+//
+
+import UIKit
+
+import SnapKit
+import Lottie
+
+final class LottieViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private let lottieView = LottieView()
+    
+    // MARK: - Life Cycles
+
+    override func loadView() {
+        super.loadView()
+
+        view = lottieView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        playAnimation()
+    }
+}
+
+extension LottieViewController {
+    func playAnimation() {
+        lottieView.lottieSplashView.play { (finished) in
+            self.moveToLoginView()
+        }
+    }
+    
+    func moveToLoginView() {
+        let nav = LoginViewController()
+        nav.modalPresentationStyle = .fullScreen
+        self.present(nav, animated: true)
+    }
+}
