88c5f25b81b676635b772869ebb9f31f8cbc7868
ahra1221 <lizze011221@gmail.com>
2023-07-15 01:03:24 +0900

    [Feat] #72 - UILabel extension 구현

--- Umbba-iOS/Umbba-iOS/Global/Extension/UILabel+.swift
+++ Umbba-iOS/Umbba-iOS/Global/Extension/UILabel+.swift
@@ -144,4 +144,15 @@ extension UILabel {
         self.attributedText = attributedString
     }
         
+    /// 라벨 일부 font 변경해주는 함수
+    /// - targerString에는 바꾸고자 하는 특정 문자열을 넣어주세요
+    /// - font에는 targetString에 적용하고자 하는 UIFont를 넣어주세요
+    func setUnderlinePartFontChange(targetString: String, font: UIFont) {
+        let fullText = self.text ?? ""
+        let range = (fullText as NSString).range(of: targetString)
+        let attributedString = NSMutableAttributedString(string: fullText)
+        attributedString.addAttribute(.font, value: font, range: range)
+        attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: range)
+        self.attributedText = attributedString
+    }
 }
