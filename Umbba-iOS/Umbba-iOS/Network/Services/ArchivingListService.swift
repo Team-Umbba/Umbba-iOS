a77c9e40b72a32c952fdef5ccd1d06632ab9c359
ahra1221 <lizze011221@gmail.com>
2023-07-16 16:21:48 +0900

    [Feat] #100 - ArchivingListService 구현

--- Umbba-iOS/Umbba-iOS/Network/Services/ArchivingListService.swift
+++ Umbba-iOS/Umbba-iOS/Network/Services/ArchivingListService.swift
@@ -18,4 +18,31 @@ final class ArchivingListService: BaseService {
 
 extension ArchivingListService {
     
+    private func makeArchivingURL(sectionId: Int) -> String {
+        let url = URLConstant.archivingURL + "/sectionId"
+        return url
+    }
+    
+    func getArchivingListAPI(completion: @escaping (NetworkResult<Any>) -> Void) {
+        let header: HTTPHeaders = NetworkConstant.hasTokenHeader
+        let dataRequest = AF.request(makeArchivingURL(sectionId: sectionId),
+                                     method: .get,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        
+        dataRequest.responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let data = response.data else { return }
+                let networkResult = self.judgeStatus(by: statusCode,
+                                                     data,
+                                                     ListEntity.self)
+                completion(networkResult)
+            case .failure:
+                completion(.networkFail)
+            }
+            
+        }
+    }
 }
