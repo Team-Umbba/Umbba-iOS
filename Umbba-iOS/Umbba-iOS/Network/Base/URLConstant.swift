9b07d76dd31a72da205cd7ca04c88df64ef88706
yujin-00 <55316673+yujin-00@users.noreply.github.com>
2023-07-16 16:18:13 +0900

    [Feat] #99 - AnswerDetail Entity 생성

--- Umbba-iOS/Umbba-iOS/Network/Base/URLConstant.swift
+++ Umbba-iOS/Umbba-iOS/Network/Base/URLConstant.swift
@@ -23,6 +23,8 @@ enum URLConstant {
     
     static let mainURL = baseURL + "/home"
     
+    static let answerURL = baseURL + "/qna/today"
+    
     // MARK: - Archiving URL
  
 }
--- Umbba-iOS/Umbba-iOS/Network/Entity/Home/Answer/AnswerEntity.swift
+++ Umbba-iOS/Umbba-iOS/Network/Entity/Home/Answer/AnswerEntity.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct AnswerEntity: Codable {
+    let qnaID: Int
+    let section, topic, opponentQuestion, myQuestion: String
+    let opponentAnswer: String?
+    let myAnswer: String
+    let isOpponentAnswer, isMyAnswer: Bool
+    let opponentUsername, myUsername: String
+
+    enum CodingKeys: String, CodingKey {
+        case qnaID = "qna_id"
+        case section, topic
+        case opponentQuestion = "opponent_question"
+        case myQuestion = "my_question"
+        case opponentAnswer = "opponent_answer"
+        case myAnswer = "my_answer"
+        case isOpponentAnswer = "is_opponent_answer"
+        case isMyAnswer = "is_my_answer"
+        case opponentUsername = "opponent_username"
+        case myUsername = "my_username"
+    }
+}
