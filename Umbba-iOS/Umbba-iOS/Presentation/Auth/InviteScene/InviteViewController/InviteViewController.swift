78a302047d3ea5bcbcdd08291a034c36a1253390
Choi YoungLyn <joeuni0129@gmail.com>
2023-07-16 18:36:52 +0900

    [Feat] #102 - 초대코드입력 API 연결

--- Umbba-iOS/Umbba-iOS/Presentation/Auth/InviteScene/InviteViewController/InviteViewController.swift
+++ Umbba-iOS/Umbba-iOS/Presentation/Auth/InviteScene/InviteViewController/InviteViewController.swift
@@ -11,6 +11,9 @@ import SnapKit
 
 final class InviteViewController: UIViewController {
     
+    // MARK: - Properties
+    var inviteCode: String = ""
+    
     // MARK: - UI Components
     
     private let inviteView = InviteView()
@@ -48,6 +51,7 @@ extension InviteViewController {
     func setDelegate() {
         inviteView.navigationdelegate = self
         inviteView.nextDelegate = self
+        inviteView.inviteDelegate = self
     }
 }
 
@@ -63,8 +67,31 @@ extension InviteViewController: NavigationBarDelegate {
 
 extension InviteViewController: NextButtonDelegate {
     func nextButtonTapped() {
-        let animationViewController =  AnimationViewController()
-        self.navigationController?.pushViewController(animationViewController, animated: true)
-        animationViewController.isReceiver = true
+        patchMatchAPI(inviteCode: self.inviteCode)
+    }
+}
+
+extension InviteViewController: InviteDelegate {
+    func sendInviteCode(inviteCode: String) {
+        self.inviteCode = inviteCode
+    }
+}
+
+extension InviteViewController {
+    func patchMatchAPI(inviteCode: String) {
+        OnBoardingService.shared.postMatchAPI(invite_code: inviteCode) { networkResult in
+            switch networkResult {
+            case .success:
+                let animationViewController =  AnimationViewController()
+                self.navigationController?.pushViewController(animationViewController, animated: true)
+                animationViewController.isReceiver = true
+            case .requestErr:
+                self.inviteView.inviteTextField.textFieldStatus = .uncorrectedType
+                self.inviteView.errorLabel.isHidden = false
+                self.inviteView.errorLabel.text = "유효하지 않은 초대코드입니다."
+            default:
+                break
+            }
+        }
     }
 }
