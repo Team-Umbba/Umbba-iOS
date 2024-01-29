803ab04c148430fce424f64a1f46325e991a4926
Choi YoungLyn <joeuni0129@gmail.com>
2024-01-30 01:20:04 +0900

    [Feat] #219 - 마이페이지 추가

---
+++ Umbba-iOS/Umbba-iOS/Presentation/Setting/MyPageScene/View/MyPageView.swift
@@ -0,0 +1,64 @@
+//
+//  MyPageView.swift
+//  Umbba-iOS
+//
+//  Created by 최영린 on 1/30/24.
+//
+
+import UIKit
+
+import SnapKit
+
+protocol SettingButtonDelegate: AnyObject {
+    func settingButtonTapped()
+}
+
+final class MyPageView: UIView {
+    
+    weak var delegate: SettingButtonDelegate?
+
+    private let navigationBarView: CustomNavigationBar = {
+        let view = CustomNavigationBar()
+        view.isSettingButtonIncluded = true
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+        setAddTarget()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+private extension MyPageView {
+    func setUI() {
+        self.backgroundColor = .White500
+    }
+    
+    func setAddTarget() {
+        navigationBarView.settingButton.addTarget(self, action: #selector(settingButtonTapped), for: .touchUpInside)
+    }
+    
+    func setLayout() {
+        self.addSubviews(navigationBarView)
+        
+        navigationBarView.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    @objc
+    func settingButtonTapped() {
+        delegate?.settingButtonTapped()
+    }
+    
+}
