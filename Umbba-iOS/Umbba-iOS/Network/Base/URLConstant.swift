335724540c477a837c4adc1cbc6ebb34c517a4be
Choi YoungLyn <joeuni0129@gmail.com>
2024-02-25 18:13:48 +0900

    [Add] #236 - MyPage API 연결에 필요한 파일 추가

--- Umbba-iOS/Umbba-iOS/Network/Base/URLConstant.swift
+++ Umbba-iOS/Umbba-iOS/Network/Base/URLConstant.swift
@@ -38,4 +38,9 @@ enum URLConstant {
  
     static let archivingURL = baseURL + "/qna/list"
     static let qnaURL = baseURL + "/qna"
+    
+    // MARK: -  MyPage URL
+       
+    static let myPageURL =  baseURL + "/user/me"
+    
 }
