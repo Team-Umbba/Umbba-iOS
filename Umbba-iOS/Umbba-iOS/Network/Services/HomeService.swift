b39be01c700eca150bdec3503e787279c63efd50
ahra1221 <lizze011221@gmail.com>
2024-02-25 13:05:43 +0900

    [Feat] #233 - patchRestartAPI 구현

--- Umbba-iOS/Umbba-iOS/Network/Services/HomeService.swift
+++ Umbba-iOS/Umbba-iOS/Network/Services/HomeService.swift
@@ -114,4 +114,27 @@ extension HomeService {
             }
         }
     }
+    
+    func patchRestartAPI(completion: @escaping (NetworkResult<Any>) -> Void) {
+        let url = URLConstant.restartURL
+        let header: HTTPHeaders = NetworkConstant.hasTokenHeader
+        let dataRequest = AF.request(url,
+                                     method: .patch,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+
+        dataRequest.responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let data = response.data else { return }
+                let networkResult = self.judgeStatus(by: statusCode,
+                                                     data,
+                                                     RestartEntity.self)
+                completion(networkResult)
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
 }
