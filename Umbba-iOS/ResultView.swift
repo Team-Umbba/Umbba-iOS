67abc58f7f55e4953512fc90e364ce7f745c2297
Choi YoungLyn <joeuni0129@gmail.com>
2024-03-02 23:58:31 +0900

    [Add] #242 - 가까워지기 결과확인 UI 파일 추가

---
+++ Umbba-iOS/ResultView.swift
@@ -0,0 +1,8 @@
+//
+//  ResultView.swift
+//  Umbba-iOS
+//
+//  Created by 최영린 on 3/2/24.
+//
+
+import Foundation
---
+++ Umbba-iOS/Umbba-iOS/Presentation/Setting/QuizScene/ViewController/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  Umbba-iOS
+//
+//  Created by 최영린 on 3/2/24.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
