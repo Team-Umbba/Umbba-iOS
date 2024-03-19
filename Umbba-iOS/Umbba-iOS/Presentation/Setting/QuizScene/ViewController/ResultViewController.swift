2ffe7a028eb91a35b12c874e8292872599912f90
Choi YoungLyn <joeuni0129@gmail.com>
2024-03-19 15:48:39 +0900

    [Feat] #244 - 가까워지기 API 3개 연결

--- Umbba-iOS/Umbba-iOS/Presentation/Setting/QuizScene/ViewController/ResultViewController.swift
+++ Umbba-iOS/Umbba-iOS/Presentation/Setting/QuizScene/ViewController/ResultViewController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 final class ResultViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    var quizEntity: QuizEntity? {
+        didSet {
+            fetchQuizData()
+        }
+    }
+    
+    // MARK: - UI Components
+    
     private let resultView = ResultView()
+    
+    // MARK: - Life Cycles
     
     override func loadView() {
         super.loadView()
@@ -20,14 +32,57 @@ final class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getQuizAPI()
         setDelegate()
     }
     
 }
 
+// MARK: - Extensions
+
 private extension ResultViewController {
     func setDelegate() {
         resultView.navigationdelegate = self
+        resultView.nextDelegate = self
+    }
+    
+    func fetchQuizData() {
+        guard let quizEntity = quizEntity else { return }
+        resultView.setDataBind(model: quizEntity)
+    }
+}
+
+// MARK: - Network
+
+extension ResultViewController {
+    func getQuizAPI() {
+        QuizService.shared.getQuizAPI { networkResult in
+            switch networkResult {
+            case .success(let data):
+                if let data = data as? GenericResponse<QuizEntity> {
+                    if let quizData = data.data {
+                        self.quizEntity = quizData
+                    }
+                }
+            case .requestErr, .serverErr:
+                self.makeAlert(title: "오류가 발생했습니다", message: "다시 시도해주세요")
+            default:
+                break
+            }
+        }
+    }
+    
+    func patchQuizNextAPI() {
+        QuizService.shared.patchQuizNextAPI { networkResult in
+            switch networkResult {
+            case .success:
+                self.navigationController?.popViewController(animated: true)
+            case .requestErr, .serverErr:
+                self.makeAlert(title: "오류가 발생했습니다", message: "다시 시도해주세요")
+            default:
+                break
+            }
+        }
     }
 }
 
@@ -39,5 +94,11 @@ extension ResultViewController: NavigationBarDelegate {
     
     func completeButtonTapped() {
         
+    }
+}
+
+extension ResultViewController: NextButtonDelegate {
+    func nextButtonTapped() {
+        patchQuizNextAPI()
     }
 }
